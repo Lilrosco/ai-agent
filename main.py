e1cee35361635b187bc6c5929b6c9b82313a5347
Lilrosco <rossey.charleston@gmail.com>
2026-01-03 01:06:39 -0500

    Added verbose arg option.

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 
 def main():
@@ -15,18 +16,25 @@ def main():
 
     parser = argparse.ArgumentParser(description="Chatbot")
     parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
     args = parser.parse_args()
+
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=args.user_prompt
+        model="gemini-2.5-flash", contents=messages
     )
 
     if response.usage_metadata:
+        user_prompt = args.user_prompt
         prompt_token_count = response.usage_metadata.prompt_token_count
         candidates_token_count = response.usage_metadata.candidates_token_count
 
-        print(f"Prompt tokens: {prompt_token_count}")
-        print(f"Response tokens: {candidates_token_count}")
+        if args.verbose:
+            print(f"User prompt: {user_prompt}")
+            print(f"Prompt tokens: {prompt_token_count}")
+            print(f"Response tokens: {candidates_token_count}")
+
         print(f"Response: {response.text}")
     else:
         raise RuntimeError("Prompt failed to return a response")
