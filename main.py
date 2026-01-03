bd142ae54b50f2f4dcf91416b5691b1baed58dc7
Lilrosco <rossey.charleston@gmail.com>
2026-01-02 23:24:32 -0500

    Takes arguments when running program.

--- main.py
+++ main.py
@@ -1,25 +1,24 @@
+import argparse
 import os
 from dotenv import load_dotenv
 from google import genai
 
 
 def main():
-    print("Hello from ai-agent!")
-
     load_dotenv()
     api_key = os.environ.get("GEMINI_API_KEY")
 
     if not api_key:
         raise Exception("Could not find API key for Gemini")
-    else:
-        print("API key loaded!")
     
     client = genai.Client(api_key=api_key)
 
-    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
     
     response = client.models.generate_content(
-        model="gemini-2.5-flash", contents=prompt
+        model="gemini-2.5-flash", contents=args.user_prompt
     )
 
     if response.usage_metadata:
